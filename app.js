9158e956b3d46f43520eac6f5a9bb205909b3493
Guillermo Gahbler <mexamericano@gmail.com>
2019-01-21 16:37:01 -0800

    testing

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-console.log('testing working')
+const rando = () =>{
+return Math.floor(Math.random()*100+1);
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
 }
 
-console.log(getRandomInt(3));
-// expected output: 0, 1 or 2
-
-console.log(getRandomInt(1));
-// expected output: 0
-
-console.log(Math.random());
-// expected output: a number between 0 and 1
+console.log(rando())
