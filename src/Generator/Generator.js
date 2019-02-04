ab229b0976559176bcab0b7dd9a5c05b531e6e82
Guillermo Gahbler <mexamericano@gmail.com>
2019-02-04 12:21:05 -0800

    attempt to deploy on github

--- src/Generator/Generator.js
+++ src/Generator/Generator.js
@@ -4,7 +4,7 @@ class Generator extends Component {
 
   // create an array in the state object
   state = {
-    values: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', "@", '#', '$', '%', '^', '&', '*', '(', ')'],
+    // values: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', "@", '#', '$', '%', '^', '&', '*', '(', ')'],
     // create a blank object this will be set as default
     letterPick: ""
   }
@@ -64,7 +64,7 @@ class Generator extends Component {
     return (
       <div>
         <button onClick={this.handleRandomAlphabet}>Button </button>
-        <p>Random letter generator: {this.state.letterPick}</p>
+        <p>Click the button to generate a random 8 character password: {this.state.letterPick}</p>
       </div>
     )
   }
